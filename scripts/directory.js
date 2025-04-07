4c42a6abe93777d4356289116c2fb74b7c21b4f0
jdalmeria-tech <alm24008@byui.edu>
2025-04-07 21:27:03 +0800

    updated the json file

--- scripts/directory.js
+++ scripts/directory.js
@@ -12,14 +12,14 @@ function displayMembers(members) {
     const card = document.createElement('article');
     card.classList.add('card');
     card.setAttribute('role', 'region');
-    card.setAttribute('aria-labelledby', `TCC Directory`);
-    card.setAttribute('aria-describedby', `TCC Directory`);
+    card.setAttribute('aria-labelledby', `Affiliates`);
+    card.setAttribute('aria-describedby', `Affiliates`);
 
     card.innerHTML = `
       <img src="${member.img}" alt="${member.name} photo" class="member-img" tabindex="0">
       <h2 id="member-${member.id}-name" tabindex="0">${member.name}</h2>
       <p id="member-${member.id}-address" tabindex="0">${member.address}</p>
-      <a href="${member.webURL}" target="_blank" tabindex="0">${member.webURL}</a>
+      <a href="${member.email}" target="_blank" tabindex="0">${member.email}</a>
       <p id="member-${member.id}-membership" tabindex="0">Membership Level: ${member.membershipLvl}</p>
     `;
 
